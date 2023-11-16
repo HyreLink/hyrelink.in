e6357e253c752641897d4e756495ade107ca7484
LokeshV790 <lokeshvazirani29@gmail.com>
2023-11-16 14:29:05 +0530

    Fixes
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -11,11 +11,11 @@ import FAQ from "@/components/FAQ";
 import Pricing from "@/components/Pricing";
 import Contact from "@/components/Contact";
 import Blog from "@/components/Blog";
-import Testimonial from "@/components/Testimonial";
+// import Testimonial from "@/components/Testimonial";
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
-  description: "This is Home for Solid Pro",
+  title: "Hyrelink.",
+  description: "Your Success, Our Passion",
   // other metadata
 };
 
@@ -31,7 +31,7 @@ export default function Home() {
       <Integration />
       <CTA />
       <FAQ />
-      <Testimonial />
+      {/* <Testimonial /> */}
       <Pricing />
       <Contact />
       <Blog />
