4b808fb837b364e36d81b153518ba8356a45e662
LokeshV790 <lokeshvazirani29@gmail.com>
2023-11-20 02:21:05 +0530

    Carrer fix
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

--- app/(site)/career/page.tsx
+++ app/(site)/career/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Career from "@/components/Career";
 
-export const career = () => {
+export const Careers = () => {
   return (
     <div className="pb-20 pt-40">
     <Career/>
   </div>
   )
 }
-export default career;
+export default Careers;
