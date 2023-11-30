3254ebeed8d1b8f910b5f0716dddb850798a1204
LokeshV790 <lokeshvazirani29@gmail.com>
2023-11-30 14:08:30 +0530

    SEO 2
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -9,7 +9,7 @@ export default function Head() {
         name="description"
         content="Unlock unparalleled business growth with HyreLink. Our comprehensive services span Digital Marketing, SEO, Website Development, App Development, Logistics, Data Analytics, AI Solutions, Event Management, and Interior Design. From concept to execution, we redefine success with innovative solutions tailored to your needs. Explore the possibilities and elevate your business with HyreLink's expertise."
       />
-      <link rel="icon" href="favicon.ico" />
+      <link rel="icon" href="/favicon.ico" />
     </>
   );
 }
