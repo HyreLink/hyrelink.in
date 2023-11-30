f48c7dc4bfe368f517fdca7f05c1349bcbedefdf
LokeshV790 <lokeshvazirani29@gmail.com>
2023-11-30 19:25:58 +0530

    seo try
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,4 @@
-// import { Metadata } from "next";
+import { Metadata } from "next";
 import Hero from "@/components/Hero";
 import Brands from "@/components/Brands";
 import Feature from "@/components/Features";
@@ -12,19 +12,19 @@ import FAQ from "@/components/FAQ";
 import Contact from "@/components/Contact";
 import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
-import Head from "./head";
+// import Head from "./head";
 
-// export const metadata: Metadata = {
-//   title: "HyreLink Solutions & Services Co. | Transforming Ideas into Digital Excellence",
-//   description: "Unlock unparalleled business growth with HyreLink. Our comprehensive services span Digital Marketing, SEO, Website Development, App Development, Logistics, Data Analytics, AI Solutions, Event Management, and Interior Design. From concept to execution, we redefine success with innovative solutions tailored to your needs. Explore the possibilities and elevate your business with HyreLink's expertise.",
-//   keywords: "Solutions, SEO, Development, Logistics, Analytics, AI, Event, Interior, Growth, Technology, Transformation, Innovation, Marketing, Creative, Professional, Excellence, Strategy, Client-Centric, Customized, Execution",
-//   // other metadata
-// };
+export const metadata: Metadata = {
+  title: "HyreLink Solutions & Services Co. | Transforming Ideas into Digital Excellence",
+  description: "Unlock unparalleled business growth with HyreLink. Our comprehensive services span Digital Marketing, SEO, Website Development, App Development, Logistics, Data Analytics, AI Solutions, Event Management, and Interior Design. From concept to execution, we redefine success with innovative solutions tailored to your needs. Explore the possibilities and elevate your business with HyreLink's expertise.",
+  keywords: "Solutions, SEO, Development, Logistics, Analytics, AI, Event, Interior, Growth, Technology, Transformation, Innovation, Marketing, Creative, Professional, Excellence, Strategy, Client-Centric, Customized, Execution",
+  // other metadata
+};
 
 export default function Home() {
   return (
     <main>
-      <Head/>
+      {/* <Head/> */}
       <Hero />
       <Brands />
       <Feature />
